b09664e3450a55a75105169c62362526c8622b4f
Joaquín  Céspedes <jcespedes@paraclinics.cl>
2024-05-14 09:29:27 -0400

    Finalizado nivel 3

--- algoApp/jsApp.js
+++ algoApp/jsApp.js
@@ -88,3 +88,92 @@ var x = [1,3,5,7];
 var y = [2,4,6];
 x[2] = y[x.length-3] + x[x[0] + y[0]];
 console.log(x);
+
+// Level 3
+//1.
+var x = [1,3,5];
+x[0] = x[2];
+x[1] = x[0];
+console.log(x);
+
+//2.
+var x = [1,3,5];
+var y = [0,5,7];
+x[0] = x[1] + y[2];
+y[1] = y[2] - x[0]; 
+console.log(x);
+
+//3.
+var x = [1,3,5,8];
+x[0] = x[x.length - 1]; 
+console.log(x); 
+
+//4.
+var x = [1,3,5,8]; 
+var temp = x[x.length -1]; 
+x[x.length - 1] = x[0]; 
+console.log(x); 
+
+//5.
+var x = [1,3,5,8]; 
+var temp = x[x.length - 1]; 
+x[x.length -1] = x[0]; 
+x[0] = temp; 
+console.log(x);
+
+//6.
+var x = [1,3,5,8]; 
+x.pop(); 
+x.push(7); 
+console.log(x); 
+
+//7.
+var x = [1,3,5];
+x[0] = 15;
+x[3] = 77;
+console.log(x);
+
+//8.
+var x = [1,3,5,8,2,-5,-8]; 
+x[0] = x[x.length - 1]; 
+x[x.length -1] = x[0]; 
+console.log(x); 
+
+//9.
+var x = [1,3,5,8,2,-5,-8]; 
+var temp = x[0]; 
+x[0] = x[x.length -1];
+x[x.length-1] = temp; 
+console.log(x);
+
+//10.
+var x = [1,3,5]; 
+x.push(7); 
+console.log(x);
+
+//11.
+var x = [2,4,6]; 
+x.push(3); 
+console.log(x);
+
+//12.
+var x = [1,3,5]; 
+var y = x[0] + x[1]; 
+x.push(y); 
+console.log(x); 
+
+//13.
+var x = [2,3,5]; 
+x.push(x[0] + x[1]); 
+console.log(x);
+
+//14.
+var x = [1,3,5];
+x.pop(); 
+console.log(x);
+
+//15.
+var x = [1,3,5]; 
+x.push(2);
+x.pop();
+console.log(x);
